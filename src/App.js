a656ed13218f7c87b71c269737e9ed9a65975ee9
rimmi <rimmi1187.cse19@chitkara.edu.in>
2023-05-11 17:19:53 +0530

    Added a file Btn.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
-import cat from './cat.jpg'
+import Btn from './components/Btn';
 import './App.css';
 
-function Logo(props){
-  const userPic = <img src = {cat} />; //embedded JSX expression {cat} in an HTML attribute 'src'
-  return userPic;
-}
 
 function App() {
   return (
-    <div>
-      <h1>Hello Everyone.</h1>
-      <Logo/>
-    </div>
+   <Btn />
   )
 }
 
---
+++ src/components/Btn.js
@@ -0,0 +1,9 @@
+function Btn(){
+
+    const clickHandler = () => console.log('clicked');
+return (
+    <button onClick={clickHandler}>Click me</button>
+)
+}
+
+export default Btn;
