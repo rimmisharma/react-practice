c23cdf91100509a217b101637b85149aac917c2b
rimmi <rimmi1187.cse19@chitkara.edu.in>
2023-05-15 11:14:13 +0530

    implemented useState hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import React from 'react';
 import './App.css';
 import Child from './components/Child';
 
 
 function App() {
-  const date = new Date()
+  const [word, setWord] = React.useState('Eat') //useState to manipulate the changed state of the component
+
+  //Function used for onClick event to display the changed event
+  function handleClick(){
+  setWord('Drink')
+  }
 
   return (
-   <div>
-    <Child message={date.toLocaleTimeString()} />
+   <div className="App">
+    <Child message={word + ' at Little Lemon'} />
+    <button onClick={handleClick}>Click here</button>
    </div>
   );
 }
