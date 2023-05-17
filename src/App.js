1f4d0dc68a665e04dd0fc67744be44bf7e385ee9
rimmi <rimmi1187.cse19@chitkara.edu.in>
2023-05-17 18:31:27 +0530

    Implemented three different ways of adding an image to my react app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import React, { useState } from 'react';
 import './App.css';
-import Homepage from './components/Homepage';
-import AboutMe from './components/AboutMe';
-import { Routes, Route } from 'react-router';
-import { Link } from 'react-router-dom';
+import wallpaper from './assets/images/211836.jpg'
 
 function App(){
+/* adding an image using a url*/
+  const imageUrl = 'https://images.unsplash.com/photo-1537824598505-99ee03483384?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NXx8fGVufDB8fHx8&w=1000&q=80';
+
   return (
-    <div className='App'>
-      <nav className='nav'>
-        <Link to='/' className='nav-item'>Homepage</Link>
-        <Link to='/about-me' className="nav-item">About Me</Link>
-      </nav>
-      <Routes>
-      <Route path="/" element={<Homepage />} />
-      <Route path="/about-me" element={<AboutMe />} />
-      </Routes>
+    <div>
+      <h1>Welcome to my page</h1>
+      <img height={200} // adding an image by import and jsx
+      src={wallpaper}
+      alt='some text'
+      ></img>
+      <br></br>
+
+      <img height={200} // adding an image using asset path
+      src={require('./assets/images/211836.jpg')}
+      alt='scenery'></img>
+      <br></br>
+
+      <img height={200}
+      src={imageUrl}
+      alt='wallpaper'></img>
     </div>
   );
 }
