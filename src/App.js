8eabbb81f0d6a2b3e6a4159065fa1c7dc6fc1694
rimmi <rimmi1187.cse19@chitkara.edu.in>
2023-05-17 18:58:38 +0530

    Implemented npm package for video: react-player

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
 import React, { useState } from 'react';
 import './App.css';
-import wallpaper from './assets/images/211836.jpg'
+import ReactPlayer from 'react-player';
 
 function App(){
-/* adding an image using a url*/
-  const imageUrl = 'https://images.unsplash.com/photo-1537824598505-99ee03483384?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NXx8fGVufDB8fHx8&w=1000&q=80';
 
+  const vidUrl='https://www.youtube.com/watch?v=cd3P3yXyx30&t=6631s';
   return (
-    <div>
-      <h1>Welcome to my page</h1>
-      <img height={200} // adding an image by import and jsx
-      src={wallpaper}
-      alt='some text'
-      ></img>
-      <br></br>
-
-      <img height={200} // adding an image using asset path
-      src={require('./assets/images/211836.jpg')}
-      alt='scenery'></img>
-      <br></br>
-
-      <img height={200}
-      src={imageUrl}
-      alt='wallpaper'></img>
+    <div className='App'>
+      <h1>React Player Example</h1>
+      <ReactPlayer className='media'
+      url={vidUrl}
+      playing={false}
+      volume={.5}
+      />
     </div>
   );
 }
