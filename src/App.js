e6aa956cb158f376715d1180da96fba795996e70
rimmi <rimmi1187.cse19@chitkara.edu.in>
2023-05-18 16:22:25 +0530

    Calculator app using useRef hook

--- src/App.js
+++ src/App.js
@@ -1,20 +1,68 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import './App.css';
 import ReactPlayer from 'react-player';
 
 function App(){
 
-  const vidUrl='https://www.youtube.com/watch?v=cd3P3yXyx30&t=6631s';
+  const inputRef = useRef(null);
+  const resultRef = useRef(null);
+  const [result, setResult] = useState(0);
+
+  const plus = (e) => {
+    e.preventDefault();
+    setResult((result) => result + Number(inputRef.current.value));
+  };
+
+  const minus = (e) => {
+    e.preventDefault();
+    setResult((result) => result - Number(inputRef.current.value));
+  };
+
+  const times = (e) => {
+    e.preventDefault();
+    setResult((result) => result * Number(inputRef.current.value));
+  };
+
+  const divide = (e) => {
+    e.preventDefault();
+    setResult((result) => result / Number(inputRef.current.value));
+  };
+
+  const resetInput = (e) => {
+    e.preventDefault();
+    inputRef.current.value = 0;
+  };
+
+  const resetResult = (e) => {
+    e.preventDefault();
+    setResult((prevVal) => prevVal * 0);
+  };
+
   return (
     <div className='App'>
-      <h1>React Player Example</h1>
-      <ReactPlayer className='media'
-      url={vidUrl}
-      playing={false}
-      volume={.5}
-      />
+      <div>
+        <h1>Simplest Working Calculator</h1>
+      </div>
+      <form>
+        <p ref={resultRef}>
+          {result}
+        </p>
+        <input
+        pattern='[0-9]'
+        ref={inputRef}
+        tyepe='number'
+        placeholder='Type a number'
+        />
+
+        <button onClick={plus}>add</button>
+        <button onClick={minus}>subtract</button>
+        <button onClick={times}>multiply</button>
+        <button onClick={divide}>divide</button>
+        <button onClick={resetInput}>reset input</button>
+        <button onClick={resetResult}>reset result</button>
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
